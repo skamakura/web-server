3f8f6be0f7d2ef6b4222c8e659c2d14631f4cdc5
skamakura <kamakura@daitohkigyo.com>
2022-02-25 17:39:26 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+
+app.unsubscribe(express.urlencoded({ extended: false}))
+// console.log(__dirname);      ルートディレクトリをコンソールに出力
+app.use(express.static(path.join(__dirname, "public")))     //ルートディレクトリのpublicフォルダを参照
+
+//npmでbody-parserをインストールした
+var bodyParser = require('body-parser')     //POSTデータ(body)取得時のエラー回避
+const { serialize } = require('v8')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.post("/api/v1/quiz", function (req, res) {
+    const answer = req.body.answer;
+    if(answer === "2") {
+        // res.send("right!")
+        res.redirect("/correct.html")
+    } else {
+        // res.send("miss!")
+        res.redirect("/wrong.html")
+
+    }
+    res.send(answer)
+})
+
+app.listen(3000)
